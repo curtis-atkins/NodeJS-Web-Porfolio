3c7dfb23c75faaac7a419b51ea001f9ecd3372e1
catkins <catkinsjr28@gmail.com>
2017-12-22 01:20:33 -0500

    fix blog page layout

--- server/controllers/projectsController.js
+++ server/controllers/projectsController.js
@@ -6,36 +6,42 @@ const projectsController ={};
 const projects = [
   {
     title: "Project 1",
+    slug: "project-1",
     tags: ["html", "css", "javascript", "php"],
     date: "01/01/2018",
     img: "/img/desktop.png"
   },
   {
     title: "Project 2",
+    slug: "project-2",
     tags: ["html", "css", "javascript", "php"],
     date: "01/01/2018",
     img: "/img/desktop.png"
   },
   {
     title: "Project 3",
+    slug: "project-3",
     tags: ["html", "css", "javascript", "php"],
     date: "01/01/2018",
     img: "/img/desktop.png"
   },
   {
     title: "Project 4",
+    slug: "project-4",
     tags: ["html", "css", "javascript", "php"],
     date: "01/01/2018",
     img: "/img/desktop.png"
   },
   {
     title: "Project 5",
+    slug: "project-5",
     tags: ["html", "css", "javascript", "php"],
     date: "01/01/2018",
     img: "/img/desktop.png"
   },
   {
     title: "Project 6",
+    slug: "project-6",
     tags: ["html", "css", "javascript", "php"],
     date: "01/01/2018",
     img: "/img/desktop.png"
